438724a3964473a7a6da9f57d8c7ebc0b6179eb6
Collin Jones <collin.g.jones34@gmail.com>
2022-03-29 15:02:52 -0500

    order exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,53 @@
+use
+employees;
+
+SELECT last_name, first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+order by last_name desc, first_name desc;
+
+SELECT count(emp_no)
+FROM employees
+WHERE last_name LIKE 'E%';
+order by emp_no desc;
+
+SELECT count(emp_no)
+FROM employees
+WHERE hire_date LIKE '%199%';
+
+SELECT count(emp_no)
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+SELECT count(emp_no)
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT count(emp_no)
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya'
+    AND gender = 'M';
+
+SELECT count(emp_no)
+FROM employees
+WHERE last_name LIKE '%E'
+   OR last_name LIKE 'E%';
+
+SELECT count(emp_no)
+FROM employees
+WHERE last_name LIKE '%E'
+  AND last_name LIKE 'E%';
+
+SELECT count(emp_no)
+FROM employees
+WHERE hire_date LIKE '%199%'
+  AND last_name LIKE '%12-25%';
+order by birth_date, hire_date desc;
+
+
+SELECT count(emp_no)
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name not LIKE '%qu%';
