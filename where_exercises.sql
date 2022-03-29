991cae3babb3772f950bef96b0cc80b620884a0a
Collin Jones <collin.g.jones34@gmail.com>
2022-03-29 13:54:14 -0500

    made some changes during the walkthrough

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,51 +1,51 @@
 use
 employees;
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
 WHERE last_name LIKE 'E%';
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
 WHERE hire_date LIKE '%199%';
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
 WHERE birth_date LIKE '%12-25';
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
 WHERE last_name LIKE '%q%';
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
-WHERE ((first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya'
-            AND gender = 'M');
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya'
+    AND gender = 'M';
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
 WHERE last_name LIKE '%E'
    OR last_name LIKE 'E%';
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
 WHERE last_name LIKE '%E'
   AND last_name LIKE 'E%';
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
 WHERE hire_date LIKE '%199%'
-AND last_name LIKE '%12-25%';
+  AND last_name LIKE '%12-25%';
 
 
-SELECT *
+SELECT count (emp_no)
 FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name LIKE '%qu%';
+  AND last_name LIKE '%qu%';
 
