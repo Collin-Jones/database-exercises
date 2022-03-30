fecb98aaa8c1e3f201ce0e5a89ecee1425cdb2be
Collin Jones <collin.g.jones34@gmail.com>
2022-03-29 20:08:04 -0500

    limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,14 @@
+use employees;
+SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name desc
+limit 10;
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '%199'
+AND birth_date LIKE '%12-25%'
+ORDER BY birth_date, hire_date desc
+limit 5 offset 45;
