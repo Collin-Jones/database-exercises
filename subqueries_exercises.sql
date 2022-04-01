08de200a66c1df050db151d5489819261b76fc7e
Collin Jones <collin.g.jones34@gmail.com>
2022-04-01 14:43:02 -0500

    made some changes during the walk through

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,7 +1,28 @@
-use employees;
-
-FROM employees
-WHERE emp_no IN(
-SELECT hire_date
+use
+employees;
+SELECT e.first_name
+FROM employees AS e
+WHERE e.hire_date IN (
+    SELECT hire_date
+    FROM employees
 WHERE emp_no LIKE '101010'
-AND hire_date LIKE '1990-10-22');
+  AND hire_date LIKE '1990-10-22'
+    );
+
+SELECT t.title
+FROM titles AS t
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+SELECT first_name, last_name
+FROM employees AS t
+WHERE gender = 'F'
+AND emp_no IN(
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > CURDATE()
+    );
+
